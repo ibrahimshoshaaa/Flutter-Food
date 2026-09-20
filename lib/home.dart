d41a6f1b76bd72d85a3220340f3439ff527042c3
ibrahimshoshaaa <brahimsosa9@gmail.com>
2026-09-20 19:32:30 +0300

    fix: preserve existing app implementation in Phase 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,31 +1,101 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_food/screens/explore_page.dart';
+import 'package:foodly/screens/explore_page.dart';
+
+import 'components/category_card.dart';
 import 'components/color_button.dart';
+import 'components/post_card.dart';
+import 'components/restaurant_landscape_card.dart';
 import 'components/theme_button.dart';
 import 'constants.dart';
+import 'models/food_category.dart';
+import 'models/post.dart';
+import 'models/restaurant.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key, required this.changeTheme, required this.changeColor, required this.colorSelected, required this.appTitle});
+  const Home({
+    super.key,
+    required this.changeTheme,
+    required this.changeColor,
+    required this.colorSelected,
+    required this.appTitle,
+  });
+
   final void Function(bool useLightMode) changeTheme;
   final void Function(int value) changeColor;
   final ColorSelection colorSelected;
   final String appTitle;
-  @override State<Home> createState() => _HomeState();
+
+  @override
+  State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  // TODO: Track current tab
   int tab = 0;
-  static const destinations = [
-    NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Explore', selectedIcon: Icon(Icons.home)),
-    NavigationDestination(icon: Icon(Icons.receipt_long_outlined), label: 'Orders', selectedIcon: Icon(Icons.receipt_long)),
-    NavigationDestination(icon: Icon(Icons.person_outline), label: 'Account', selectedIcon: Icon(Icons.person)),
+
+  // TODO: Define tab bar destinations
+  List<NavigationDestination> appBarDestinations = const [
+    NavigationDestination(
+      icon: Icon(Icons.home_outlined),
+      label: 'Explore',
+      selectedIcon: Icon(Icons.home),
+    ),
+    NavigationDestination(
+      icon: Icon(Icons.list_outlined),
+      label: 'Orders',
+      selectedIcon: Icon(Icons.list),
+    ),
+    NavigationDestination(
+      icon: Icon(Icons.person_2_outlined),
+      label: 'Account',
+      selectedIcon: Icon(Icons.person),
+    ),
   ];
-  @override Widget build(BuildContext context) {
-    final pages = [const ExplorePage(), const Center(child: Text('Orders')), const Center(child: Text('Account'))];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Define pages
+    final pages = [
+      ExplorePage(),
+      const Center(
+          child: Text('Order Page', style: TextStyle(fontSize: 32.0)),
+      ),
+      const Center(
+        child: Text('Account Page', style: TextStyle(fontSize: 32.0)),
+      ),
+    ];
+
     return Scaffold(
-      appBar: AppBar(title: Text(widget.appTitle), actions: [ThemeButton(changeThemeMode: widget.changeTheme), ColorButton(changeColor: widget.changeColor, colorSelected: widget.colorSelected)]),
+      appBar: AppBar(
+        title: Text(widget.appTitle),
+        elevation: 4.0,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        actions: [
+          ThemeButton(
+            changeThemeMode: widget.changeTheme,
+          ),
+          ColorButton(
+            changeColor: widget.changeColor,
+            colorSelected: widget.colorSelected,
+          ),
+        ],
+      ),
+      // TODO: Switch between pages
       body: IndexedStack(index: tab, children: pages),
-      bottomNavigationBar: NavigationBar(selectedIndex: tab, onDestinationSelected: (index) => setState(() => tab = index), destinations: destinations),
+      // TODO: Add bottom navigation bar
+      // 1
+      bottomNavigationBar: NavigationBar(
+        // 2
+        selectedIndex: tab,
+        // 3
+        onDestinationSelected: (index) {
+          setState(() {
+            tab = index;
+          });
+        },
+        // 4
+        destinations: appBarDestinations,
+      ),
     );
   }
 }
