d7597c0adb82146ec33301678068a62b12566ec8
ibrahimshoshaaa <brahimsosa9@gmail.com>
2026-09-20 19:26:43 +0300

    chore: establish Flutter Food production foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'constants.dart';
 import 'home.dart';
 
-void main() {
-  runApp(const Foodly());
+void main() { WidgetsFlutterBinding.ensureInitialized(); runApp(const FlutterFoodApp()); }
+
+class FlutterFoodApp extends StatefulWidget {
+  const FlutterFoodApp({super.key});
+  @override State<FlutterFoodApp> createState() => _FlutterFoodAppState();
 }
 
-class Foodly extends StatefulWidget {
-
-  const Foodly({super.key});
-
-  @override
-  State<Foodly> createState() => _FoodlyState();
-}
-
-class _FoodlyState extends State<Foodly> {
-  // TODO: Setup default theme
+class _FlutterFoodAppState extends State<FlutterFoodApp> {
   ThemeMode themeMode = ThemeMode.light;
- // Manual theme toggle
-  ColorSelection colorSelected = ColorSelection.pink;
-
-  // TODO: Add changeTheme above here
-  void changeThemeMode(bool useLightMode) {
-    setState(() {
-      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
-    });
-  }
-
-  void changeColor(int value) {
-    setState(() {
-      colorSelected = ColorSelection.values[value];
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    const appTitle = 'Foodly';
-    // TODO: Setup default theme
-    //3
+  ColorSelection colorSelected = ColorSelection.blue;
+  void changeThemeMode(bool useLightMode) => setState(() { themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark; });
+  void changeColor(int value) => setState(() { colorSelected = ColorSelection.values[value]; });
+  @override Widget build(BuildContext context) {
+    const appTitle = 'Flutter Food';
     return MaterialApp(
       title: appTitle,
-      debugShowCheckedModeBanner: false, // Uncomment to remove Debug banner
-      // TODO: Add theme
+      debugShowCheckedModeBanner: false,
       themeMode: themeMode,
-      theme: ThemeData(
-        colorSchemeSeed: colorSelected.color,
-        useMaterial3: true,
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        colorSchemeSeed: colorSelected.color,
-        useMaterial3: true,
-        brightness: Brightness.dark,
-      ),
-      // TODO: Replace Scaffold with Home widget
-      // 4
-      home: Home(
-        appTitle: appTitle, //todo: stef delete this?
-
-        changeTheme: changeThemeMode,
-        changeColor: changeColor,
-        colorSelected: colorSelected,
-      ),
+      theme: ThemeData(colorSchemeSeed: colorSelected.color, useMaterial3: true),
+      darkTheme: ThemeData(colorSchemeSeed: colorSelected.color, useMaterial3: true, brightness: Brightness.dark),
+      home: Home(appTitle: appTitle, changeTheme: changeThemeMode, changeColor: changeColor, colorSelected: colorSelected),
     );
   }
 }
